fe40b86e14179792ced706cd8088dd5dd402430b
FanTik1011 <140693025+FanTik1011@users.noreply.github.com>
2025-05-04 11:47:37 +0300

    ;

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
 from app import app
 import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
